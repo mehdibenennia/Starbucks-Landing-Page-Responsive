670e3a5e6cbaf3c0bc65fbf376f42e9b49063b39
El Mehdi Benennia <82849033+mehdibenennia@users.noreply.github.com>
2021-10-02 13:18:49 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,17 @@
+const drink = document.querySelector('.drink');
+const colors = ['#017143', '#eb7495', '#d752b1'];
+const menuToggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.navigation');
+const btnDrinks = document.querySelectorAll(".footer > img");
 
+for (let i = 0; i < btnDrinks.length; i++) {
+    btnDrinks[i].addEventListener('click', function () {
+        drink.scrollLeft = drink.offsetWidth * i;
+        document.body.style.setProperty('--drink', colors[i]);
+    });
+}
 
-
-
+document.getElementById("menu").addEventListener('click', function () {
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+});
